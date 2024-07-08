d55d8b37be50c9eb56bd282b93f18a41feca7fda
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-07 21:41:52 -0500

    Creando archivo principal para implementar funciones de la plantilla

---
+++ calculator.c
@@ -0,0 +1,25 @@
+#include "calculator.h"
+
+double add(double a, double b){
+    return 0;
+}
+
+double subtract(double a, double b){
+    return 0;
+}
+
+double multiply(double a, double b){
+    return 0;
+}
+
+double divide(double a, double b){
+    return 0;
+}
+
+double pow(double a, double b){
+    return 0;
+}
+
+double sqrt(double a){
+    return 0;
+}
