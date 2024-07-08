199c974df4fe93b95acb2cae7218abc5aee07197
Jackson Merma <jmermap@unsa.edu.pe>
2024-07-07 21:41:03 -0500

    Definiendo un template básico para funciones de una calculadora

---
+++ calculator.h
@@ -0,0 +1,46 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+/*
+ * Add two doubles
+ * Example:
+ * 	add(2.0, 3.5) = 5.5
+ */
+double add(double, double);
+
+/*
+ * Subtract two doubles
+ * Example:
+ * 	subtract(2.0, 3.5) = -1.5
+ */
+double subtract(double, double);
+
+/*
+ * Multiply two doubles
+ * Example:
+ * 	multiply(2.0, 3.5) = 7.0
+ */
+double multiply(double, double);
+
+/*
+ * Divide two doubles
+ * Example:
+ * 	divide(2.0, 3.5) = 0.571428
+ */
+double divide(double, double);
+
+/*
+ * Pow result of two doubles
+ * Example:
+ * 	divide(2.0, 3.5) = 11.313708
+ */
+double pow(double, double);
+
+/*
+ * Square root of a double 
+ * Example:
+ * 	sqrt(2.0) = 1.414213
+ */
+double sqrt(double);
+
+#endif
